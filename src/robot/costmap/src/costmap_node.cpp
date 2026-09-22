2c492da2bc89d7b9e967fe8c01bf670790fd8a36
Gabriel Nakamoto <gabriel@nakamoto.ca>
2026-09-22 12:11:34 -0400

    fix boundary overflow

--- src/robot/costmap/src/costmap_node.cpp
+++ src/robot/costmap/src/costmap_node.cpp
@@ -11,7 +11,7 @@ CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->
 }
 
 void CostmapNode::processLidar(const std::shared_ptr<sensor_msgs::msg::LaserScan> scan) {
-  const std::size_t n_cells = CostmapNode::grid_size * CostmapNode::grid_resolution;
+  const int n_cells = CostmapNode::grid_size * CostmapNode::grid_resolution;
   std::vector<int8_t> occupancy_grid(n_cells*n_cells, 0);
   std::vector<std::size_t> detected;
 
@@ -20,37 +20,33 @@ void CostmapNode::processLidar(const std::shared_ptr<sensor_msgs::msg::LaserScan
     double theta = scan->angle_min + i * scan->angle_increment, range = scan->ranges[i];
     if (range < scan->range_max && range > scan->range_min) {
       double x_proj = range * std::cos(theta), y_proj = range * std::sin(theta);
-      int x_idx = static_cast<int>(x_proj * grid_resolution) + (n_cells / 2);
-      int y_idx = static_cast<int>(y_proj * grid_resolution) + (n_cells / 2);
+      int x_idx = x_proj * (int)grid_resolution + (n_cells/2);
+      int y_idx = y_proj * (int)grid_resolution + (n_cells/2);
 
-      if (y_idx > n_cells || x_idx > n_cells) continue;
+      if (x_idx < 0 || y_idx < 0 || y_idx >= n_cells || x_idx >= n_cells) continue;
 
       occupancy_grid[y_idx * n_cells + x_idx] = 100;
       detected.push_back(y_idx * n_cells + x_idx);
     }
   }
 
-  RCLCPP_INFO(this->get_logger(), "%d object points detected", detected.size());
-
   // propogate propability based on proximity
   const int inflate_dist = CostmapNode::inflation_radius * CostmapNode::grid_resolution;
   for (const std::size_t &cell : detected) {
-    std::size_t x = cell % n_cells, y = cell / n_cells;
+    const int x = static_cast<int>(cell) % n_cells, y = static_cast<int>(cell) / n_cells;
 
     for (int dy=-inflate_dist; dy<=inflate_dist; ++dy) {
       for (int dx=-inflate_dist; dx<=inflate_dist; ++dx) {
         int nx = x+dx, ny = y+dy;
-        if (nx < 0 || ny < 0) continue;
-
-        size_t inf_cell = ny*n_cells + nx;
-        if (inf_cell >= (n_cells*n_cells)) continue;
+        if (nx < 0 || ny < 0 || nx >= n_cells || ny >= n_cells) continue;
 
         // NOTE: rate is done in cell space not meters
         const std::size_t eudist = std::sqrt(dy*dy + dx*dx);
+        if (eudist > inflate_dist) continue;
         uint8_t cost = static_cast<uint8_t>(CostmapNode::inflation_max_cost * (1.0 - ((double)eudist/inflate_dist)));
 
-        if (cost > occupancy_grid[inf_cell])
-          occupancy_grid[inf_cell] = cost;
+        if (cost > occupancy_grid[ny*n_cells + nx])
+          occupancy_grid[ny*n_cells + nx] = cost;
       }
     }
   }
@@ -61,12 +57,10 @@ void CostmapNode::processLidar(const std::shared_ptr<sensor_msgs::msg::LaserScan
   grid_packet.info.height = n_cells;
   grid_packet.info.resolution = 1.0f / (float)CostmapNode::grid_resolution;
   grid_packet.info.origin.position.x = -(double)grid_size / 2.0;
-  grid_packet.info.origin.position.x = -(double)grid_size / 2.0;
-  grid_packet.info.origin.orientation.w = 1.0;
+  grid_packet.info.origin.position.y = -(double)grid_size / 2.0;
 
   grid_packet.header.stamp = this->get_clock()->now();
   grid_packet.header.frame_id = scan->header.frame_id;
-
 
   grid_packet.data = std::move(occupancy_grid);
 
